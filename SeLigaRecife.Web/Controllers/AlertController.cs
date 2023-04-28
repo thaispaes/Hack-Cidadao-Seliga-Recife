b214e41f2005d332b0540f6045a2b33e22d36384
Davi Freitas <davimello14@gmail.com>
2023-04-28 02:24:02 -0300

    adjusts

---
+++ SeLigaRecife.Web/Controllers/AlertController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeLigaRecife.Web.Context;
+
+namespace SeLigaRecife.Web.Controllers;
+
+public class AlertController : Controller
+{
+    public SeLigaRecifeDbContext _context;
+
+    public AlertController(SeLigaRecifeDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var alerts = _context.Alerts.ToList();
+        return Json(alerts);
+    }
+}
